e979d11ab4f53b91cbceb300e4bb82ddd50aa285
Minskoleopc <minskoleopc@gmail.com>
2023-05-22 08:59:29 +0530

    added

---
+++ script.js
@@ -0,0 +1,55 @@
+// let and const 
+
+let  x = 100
+console.log(x)
+x = 300
+console.log(x)
+
+const j = 900
+console.log(j)
+
+
+//j = 8900
+
+// Arithmetic operation - 
+// Addition - +
+// Subtraction - -
+// Multiplication - *
+// Division - /
+
+
+let s = 10
+let t = 5
+
+console.log(s+t)
+console.log(s-t)
+console.log(s*t)
+console.log(s/t)
+console.log(s%t)
+
+
+let q = 8
+let r = 4
+
+console.log(q+r)
+console.log(q-r)
+console.log(q*r)
+console.log(q/r)
+console.log(q%r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

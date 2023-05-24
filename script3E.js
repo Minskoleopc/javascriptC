743f6fcb961c8a35ccf6bac5485712aed8f13210
Minskoleopc <minskoleopc@gmail.com>
2023-05-24 21:01:43 +0530

    added

---
+++ script3E.js
@@ -0,0 +1,108 @@
+// Introvert         and Extrovert
+
+// calm                      loud
+// less outing               more social
+// less social               more outing
+
+// Human
+// Property - height , age , weight 
+// Method - walk() , talk()
+
+// Vehicle 
+// Property - color , type ,   model 
+// Method  - start() , stop()
+
+
+// Bank 
+// Property - bal , accNo , accType , IFSC code , branch 
+// Method - deposit()  , deposit()
+
+let x = 10
+console.log(x)
+console.log(typeof x)
+// 10 , -10 , 0 ,89.7,-78.8 
+
+let y = true
+console.log(y)
+console.log(typeof y)
+// true boolean
+
+let z = "chinmay"
+console.log(z)
+console.log(typeof z)
+// " " , "123"  , "123123","2@@@@@@" , "$$$$", "aaaa"
+
+
+// comparison operator
+
+console.log(typeof 10) // number
+console.log(typeof '10') // string
+
+
+// < , > , <= , >=
+// == , !=   -- value
+// === , !==  -- value and type
+console.log(2 == '2')
+console.log(2 === '2')
+console.log(2 != '2')
+console.log(2 !== '2')
+console.log(2 === '2')
+console.log(9 === 9)
+console.log(5 > 6)
+console.log(5 < 6)
+console.log(5 >= 5)
+console.log(5 <= 5)
+console.log(5 <= 6)
+console.log(5 >= 6)
+
+// logical operator 
+
+
+// AND   --- &&
+// true  && true  ===> true
+// false && true  ===> false
+// true  && false ===> false
+// false && false ===> false
+
+console.log(6 == 6 && 7 == 7)
+console.log(6 != 6 && 7 == 7)
+console.log(6 == 6 && 7 != 7)
+console.log(6 != 6 && 7 != 7)
+
+
+// OR  --- ||
+// true  || true  ===> true
+// false || true  ===> true
+// true  || false ===> true
+// false || false ===> false
+
+console.log(6 == 6 || 7 == 7)
+console.log(6 != 6 || 7 == 7)
+console.log(6 == 6 || 7 != 7)
+console.log(6 != 6 || 7 != 7)
+
+// NOT - !
+
+// true -- false
+// false -- true
+
+console.log(!(7 == 7))
+console.log(!(7 === '7'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

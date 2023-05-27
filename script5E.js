648a728fa176df9bc4740dc283daaff32de37330
Minskoleopc <minskoleopc@gmail.com>
2023-05-27 20:54:03 +0530

    added

---
+++ script5E.js
@@ -0,0 +1,89 @@
+
+// switch case without break statement
+// let city = "bhopal"
+// switch(city){
+//     case "pune":
+//         console.log("MH")
+//     case "bhopal":
+//         console.log('MP')
+//     case "jaipur":
+//         console.log("RJ")
+//     case 'lucknow':
+//         console.log("UP")
+//     default:
+//         console.log('Incorrect city name')
+// }
+
+
+// switchcase with break statement
+// program 2
+
+// let city2 = "jaipur"
+
+// switch(city2){
+//     case "pune":
+//         console.log('MH')
+//         break
+//     case 'indore':
+//         console.log('MP')
+//         break
+//     case 'lucknow':
+//         console.log('UP')
+//         break
+//     case 'jaipur':
+//         console.log('RJ')
+//         break
+//     default:
+//         console.log('Incorrect city name')
+// }
+
+// program3
+
+let city3 = "sagar"
+switch(city3){
+    case "pune":
+    case "nagpur":
+        console.log('MH')
+        break
+    case 'indore':
+    case 'bhopal':
+    case 'sagar':
+        console.log('MP')
+        break
+    case 'lucknow':
+    case 'varanasi':
+        console.log('UP')
+        break
+    case 'jaipur':
+    case 'udaipur':
+        console.log('RJ')
+        break
+    default:
+        console.log('Incorrect city name')
+}
+
+// truthy value and falsy values
+
+// truthy -- -1, 1,"A", " ", "chinmay", true , 2==2,321,{} , []
+// 0 , undefined , null, NaN,  "" , false , 9 >10
+
+if( 9 > 10){
+    console.log('hello')
+}
+else {
+    console.log('Bye')
+}
+
+// 8:30 am
+
+
+
+
+
+
+
+
+
+
+
+

e75d6e8b172891fabd057b9aab082a57442e6125
Minskoleopc <minskoleopc@gmail.com>
2023-05-27 08:53:48 +0530

    added

---
+++ script5.js
@@ -0,0 +1,61 @@
+// switch case without break statement
+
+// let city = "jaipur"
+
+// switch(city){
+//     case 'pune':
+//         console.log('MH')
+//     case 'jaipur':
+//         console.log('RJ')
+//     case 'bhopal':
+//         console.log('MP')
+//     case 'luknow':
+//         console.log('UP')
+//     default:
+//         console.log('incorrect name')
+// }
+
+// program2
+// let city2 = "goa"
+// switch (city2) {
+//     case 'pune':
+//         console.log('MH')
+//         break
+//     case 'jaipur':
+//         console.log('RJ')
+//         break
+//     case "lucknow":
+//         console.log('UP')
+//         break
+//     case 'bhopal':
+//         console.log('MP')
+//         break
+//     default:
+//         console.log('Incorrect cityName')
+// }
+
+// let city3 = "indore"
+// switch (city3) {
+//     case 'pune':
+//     case 'nagpur':
+//         console.log('MH')
+//         break
+//     case 'jaipur':
+//     case 'udaipur':
+//         console.log('RJ')
+//         break
+//     case "lucknow":
+//     case 'varanasi':
+//         console.log('UP')
+//         break
+//     case 'bhopal':
+//     case 'indore':
+//         console.log('MP')
+//         break
+//     default:
+//         console.log('Incorrect cityName')
+// }
+
+
+
+

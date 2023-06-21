3ecc0d54d7c39ebc8d625a8bb32fa7b987c48422
Minskoleopc <minskoleopc@gmail.com>
2023-06-21 09:04:55 +0530

    added

---
+++ script16.js
@@ -0,0 +1,111 @@
+// Object -- properties and methods
+let firstName = "chinmay"
+console.log(firstName)
+
+let q1 = firstName.length
+console.log(q1)
+
+
+// 10 methods
+
+// Method ----> 
+// action 
+// return type 
+
+
+let  firstN = "ram";
+let q22  = firstN.toUpperCase()
+console.log(q22)
+
+
+let lastN = "DESHPANDE"
+let q33 = lastN.toLowerCase()
+console.log(q33)
+
+let city = "pune "
+let q44 = city.includes('p')
+let q55 = city.includes('ne ')
+console.log(q44)
+console.log(q55)
+
+
+// 0    1    2   3    4   5   6
+// c    h    e   n    n   a   i
+let city2 = "chennai"
+let q66 = city2.indexOf('e')
+console.log(q66)
+let q77 = city2.indexOf('E')
+console.log(q77)
+
+
+// trim()
+
+let city3 = " goa "
+console.log(city3.length)
+let q88 = city3.trim()
+console.log(q88.length)
+
+
+// trimStart()
+let city4 = " goa "
+console.log(city4.length)
+let q99 = city4.trimStart()
+console.log(q99.length)
+
+
+// trimEnd()
+let city5 = "goa "
+console.log(city5.length)
+let q100 =  city5.trimEnd()
+console.log(q100.length)
+
+// startsWith()
+
+let city6 = "Nagpur"
+let q111 = city6.startsWith("Nagpur")
+let q112 = city6.startsWith("N")
+console.log(q111)
+console.log(q112)
+
+
+// endsWith()
+let city7 = "Bhopal"
+let q113 = city7.endsWith("pal")
+let q114 = city7.endsWith("l")
+console.log(q113)
+console.log(q114)
+
+//replace
+let city8 = "I am learning javascript"
+let q115 = city8.replace('javascript','python')
+console.log(q115)
+
+
+// toLowerCase()
+// toUpperCase()
+// indexOf()
+// includes()
+// trimStart()
+// trim()
+// trimEnd()
+// startsWith()
+// endsWith()
+// replace()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
